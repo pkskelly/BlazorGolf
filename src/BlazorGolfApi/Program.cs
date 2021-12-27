44c0258887558280588d17cf36953c93a752ee91
Pete Skelly <peteskelly@gmail.com>
2021-12-27 12:31:18 -0500

    Closes #10. Added base project for WebAPI. (#14)

---
+++ src/BlazorGolfApi/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+    
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowEveryone",
+        options =>
+        {
+            options.WithOrigins("*").
+                    AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
